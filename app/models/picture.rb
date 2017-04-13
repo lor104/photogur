c7ca808e61db72efb25623b5f37383c1102a4219
lor104 <lauren.rosentzveig@gmail.com>
2017-04-13 16:53:17 -0400

    write validations and test them

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,11 @@
 class Picture < ApplicationRecord
+
+  validates :artist,  presence: true
+
+  validates :title, length: {in: 3..20, too_short: "Between 3 and 20 is the number of characters allowed in a title"}
+  # validates :title, length: {maximum: 20, too_long: "#{count} is the maximum number of characters allowed in a title"}
+
+  validates :url, presence: true
+  validates :url, uniqueness: true
+
 end
